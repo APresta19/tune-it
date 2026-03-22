06544cf90c6cb014faca86c9b2ea19f456c4d024
Andrew <103297057+APresta19@users.noreply.github.com>
2026-03-22 13:41:02 -0400

    Updated DB for Render and Updated Pool

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,12 +3,16 @@ dotenv.config();
 
 import { Pool } from "pg";
 
-const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-  password: String(process.env.PG_PASSWORD)
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+        : {
+            host: process.env.PG_HOST,
+            port: Number(process.env.PG_PORT),
+            database: process.env.PG_DATABASE,
+            user: process.env.PG_USER,
+            password: String(process.env.PG_PASSWORD),
+          }
+);
 
 export default pool;
